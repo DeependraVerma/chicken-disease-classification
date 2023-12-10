209417af6805e3221c796f0969e50315a47f4409
Deependra Verma <deependra.verma00@gmail.com>
2023-12-10 10:02:36 +0530

    Data Ingestion

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # chicken-disease-classification
+
+## Workflows
+
+1. Update config.yaml
+2. Update secrets.yaml [Optional]
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager inside the src config
+6. Update tehe components
+7. Update the pipeline
+8. Update the main.py
+9. Update the dvc.yaml
---
+++ main.py
@@ -0,0 +1,13 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========")
+except Exception as e:
+        logger.exception(e)
+        raise(e)
